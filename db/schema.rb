9372b686ec800ba844f70bcdb59ec2dd2dba3a3c
Garrett <garrett.anglin@ymail.com>
2024-02-08 01:22:49 +0000

    removed first and last name attribute from members

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_06_195120) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_08_011856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,8 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_06_195120) do
   end
 
   create_table "members", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "full_name"
