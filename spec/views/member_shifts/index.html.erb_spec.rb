b637361c9020ba9e8b96e58e323de88582999dab
Harrison Held <harrydheld@gmail.com>
2024-02-11 14:19:12 -0600

    Roles controller tested

--- spec/views/member_shifts/index.html.erb_spec.rb
+++ spec/views/member_shifts/index.html.erb_spec.rb
@@ -28,6 +28,6 @@ RSpec.describe('member_shifts/index', type: :view) do
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select cell_selector, text: Regexp.new('Title'.to_s), count: 2
     assert_select cell_selector, text: Regexp.new(2.to_s), count: 6
-    assert_select cell_selector, text: Regexp.new(3.to_s), count: 6
+    assert_select cell_selector, text: Regexp.new(3.to_s), count: 2
   end
 end
