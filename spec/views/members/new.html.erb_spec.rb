a08a54dc9daedb33eb5e0447b200de86bc6e7f28
Harrison Held <harrydheld@gmail.com>
2024-02-11 13:54:28 -0600

    100% coverage for spec/views/members/new.html.erb_spec.rb

--- spec/views/members/new.html.erb_spec.rb
+++ spec/views/members/new.html.erb_spec.rb
@@ -1,16 +1,20 @@
-# frozen_string_literal: true
-
+# spec/views/members/new.html.erb_spec.rb
 require 'rails_helper'
 
-RSpec.describe('members/new', type: :view) do
+RSpec.describe 'members/new', type: :view do
   before do
     assign(:member, Member.new(
-                      role: nil,
-                      full_name: 'MyString',
-                      email: 'test@gmail.com',
-                      password: 'test12'
-                    )
-    )
+      role: 'admin',
+      full_name: 'MyString',
+      email: 'test@gmail.com',
+      password: 'test12'
+    ))
+    render
   end
 
+  it 'renders the new member form' do
+    expect(rendered).to have_selector('form[action="/members"]', count: 1)
+    expect(rendered).to have_field('member_full_name', with: 'MyString')
+    expect(rendered).to have_field('member_role', with: 'admin')
+  end
 end
