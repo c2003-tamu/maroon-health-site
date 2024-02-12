49c57f01ce5e52389482f0577277eb133e7f70b1
c2003-tamu <c2003@tamu.edu>
2024-02-12 16:30:17 +0000

    fixed migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_09_154251) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_12_121213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.integer "ideal_volunteers"
-    t.integer "ideal_officers"
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.integer "member_id"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,13 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_09_154251) do
     t.string "name"
     t.integer "stock"
     t.text "notes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "member_shifts", force: :cascade do |t|
-    t.integer "member_id"
-    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
