5a1e06bdd1da64bfaed08b3029bec30e9f501a4c
Harrison Held <harrydheld@gmail.com>
2024-02-16 18:55:57 -0600

    Added sign in to tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -33,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort(e.to_s.strip)
 end
 RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
 
