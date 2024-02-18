00f18ef3846c4219cf72b3a63b037e227e5edff0
c2003-tamu <c2003@tamu.edu>
2024-02-18 02:30:32 +0000

    small fix to not give people the volunteer rank on their first login

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Member < ApplicationRecord
-  validates :role, presence: true, inclusion: { in: %w[admin volunteer], message: '%<value>s is not a valid role' }
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable,
@@ -15,7 +13,6 @@ class Member < ApplicationRecord
       member.password = Devise.friendly_token[0, 20]
       member.full_name = auth.info.name
       member.avatar_url = auth.info.image
-      member.role = 'volunteer'
     end
   end
 
