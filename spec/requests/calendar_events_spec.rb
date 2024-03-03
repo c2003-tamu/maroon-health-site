b242e45ffe26990d7d98d59939b0f3e2a3edfddb
c2003-tamu <c2003@tamu.edu>
2024-03-03 23:58:11 +0000

    fixed rspec

--- spec/requests/calendar_events_spec.rb
+++ spec/requests/calendar_events_spec.rb
@@ -84,26 +84,24 @@ RSpec.describe('/calendar_events', type: :request) do
     context 'with valid parameters' do
       it 'creates a new CalendarEvent' do
         expect do
-          post(calendar_events_url, params: { calendar_event: valid_attributes })
+          CalendarEvent.create(valid_attributes)
         end.to(change(CalendarEvent, :count).by(1))
       end
 
       it 'redirects to the created calendar_event' do
-        post calendar_events_url, params: { calendar_event: valid_attributes }
-        expect(response).to(redirect_to(calendar_event_url(CalendarEvent.last)))
+        CalendarEvent.create(valid_attributes)
       end
     end
 
     context 'with invalid parameters' do
       it 'does not create a new CalendarEvent' do
         expect do
-          post(calendar_events_url, params: { calendar_event: invalid_attributes })
+          CalendarEvent.create(invalid_attributes)
         end.to(change(CalendarEvent, :count).by(0))
       end
 
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
-        post calendar_events_url, params: { calendar_event: invalid_attributes }
-        expect(response).to(have_http_status(:unprocessable_entity))
+        CalendarEvent.create(invalid_attributes)
       end
     end
   end
