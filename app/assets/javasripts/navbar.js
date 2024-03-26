5a9456cbe2f9e5f4d5c8764619706ac1365246b8
c2003-tamu <c2003@tamu.edu>
2024-03-26 17:30:48 +0000

    trying to get this dang navbar to work

--- app/assets/javasripts/navbar.js
+++ app/assets/javasripts/navbar.js
@@ -1,14 +1,20 @@
 // app/assets/javascripts/navbar.js
-document.addEventListener('turbolinks:load', function() {
+function initializeNavbarToggle() {
   const navbarToggle = document.getElementById('navbar-toggle');
   const navbarLinks = document.getElementById('navbar-links');
 
   navbarToggle.addEventListener('click', function() {
     navbarLinks.classList.toggle('active');
   });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initializeNavbarToggle();
 });
 
-
+document.addEventListener('turbolinks:load', function() {
+  initializeNavbarToggle();
+});
 
 
 
