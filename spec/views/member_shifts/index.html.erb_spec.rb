5be8e30b7185548089442e1aff0b8a2bd8fa7328
Sean-Sayce <98335624+Sean-Sayce@users.noreply.github.com>
2024-03-30 16:31:28 -0500

    Fixed rspec

--- spec/views/member_shifts/index.html.erb_spec.rb
+++ spec/views/member_shifts/index.html.erb_spec.rb
@@ -2,8 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe('events/index', type: :view) do
+RSpec.describe 'events/index', type: :view do
   before do
+    allow(view).to receive(:current_member).and_return(admin_member)
+    
     assign(:events, [
       Event.create!(
         title: 'Title',
@@ -25,9 +27,7 @@ RSpec.describe('events/index', type: :view) do
         start_time: 1.day.from_now.strftime('%Y-%m-%d %H:%M:%S'),
         end_time: (1.day.from_now + 2.hours).strftime('%Y-%m-%d %H:%M:%S')
       )
-    ]
-    )
-    sign_in admin_member
+    ])
   end
 
   let(:admin_member) do
@@ -37,13 +37,6 @@ RSpec.describe('events/index', type: :view) do
       role: 'admin'
     )
   end
-  let(:volunteer_member) do
-    Member.create!(
-      email: 'ilovevolunteering@gmail.com',
-      password: 'ilovehelpingpeople123',
-      role: 'volunteer'
-    )
-  end
 
   it 'renders a list of events' do
     render
@@ -51,3 +44,4 @@ RSpec.describe('events/index', type: :view) do
     assert_select cell_selector, text: Regexp.new('Title'.to_s), count: 2
   end
 end
+
